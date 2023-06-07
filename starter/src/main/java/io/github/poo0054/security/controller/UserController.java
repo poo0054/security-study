4da17234ca7317ca029326bfd526eff1e3877de5
poo0054 <poo0054@outlook.com>
2023-06-07 10:38:29 +0800

    init

--- starter/src/main/java/io/github/poo0054/security/controller/UserController.java
+++ starter/src/main/java/io/github/poo0054/security/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,7 +22,7 @@ public class UserController {
 	private UsersServiceImpl userInfoService;
 
 	@GetMapping("/get")
-//	public Users getUser(String username, CsrfToken token, @AuthenticationPrincipal Users customUser, @CurrentSecurityContext Authentication authentication) {
+//	public Users getUser(String username,@CsrfToken CsrfToken token, @AuthenticationPrincipal Users customUser, @CurrentSecurityContext Authentication authentication) {
 	public Users getUser(String username, @AuthenticationPrincipal Users customUser, @CurrentSecurityContext SecurityContext securityContext) {
 		return userInfoService.getUsers(username);
 	}
@@ -29,7 +31,7 @@ public class UserController {
 	@PreAuthorize("hasRole('sys')") // 只能user角色才能访问该方法
 //	@PreAuthorize("hasAuthority('menu:sys')") // 只能user角色才能访问该方法
 //	@PreAuthorize("hasAnyRole('user')") // 只能user角色才能访问该方法
-	public Users getUser(String username) {
+	public Users getUser(@PathVariable @RequestBody String username) {
 		return userInfoService.getUsers(username);
 	}
 
